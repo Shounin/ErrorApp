f1a95558aa4753846cd1243aeea771fc6ba2387d
Garviell <faustusxi@gmail.com>
2014-03-14 15:01:49 +0000

    I CANT REMEMB3R WHAT I DID

---
+++ ErrorApp/Code/LogMedia.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ErrorApp.Code
+{
+    public class LogMedia
+    {
+        //WHY YOU SO BAD?
+        public virtual void LogMessage(string message){ }
+    }
+}
---
+++ ErrorApp/Code/OutputWindowLogMedia.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.ComponentModel;
+
+namespace ErrorApp.Code
+{
+    public class OutputWindowLogMedia : LogMedia
+    {
+        //RAWR
+        public override void LogMessage(string message)
+        {
+
+        }
+    }
+}
