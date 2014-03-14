97b584f87ff1a0439316cccfe9b5bf74a903d545
Shounin <gstyrmir@gmail.com>
2014-03-14 13:17:33 +0000

    Initial Commit

---
+++ ErrorApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ErrorApp.MvcApplication" Language="C#" %>
---
+++ ErrorApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ErrorApp.Startup))]
+namespace ErrorApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
