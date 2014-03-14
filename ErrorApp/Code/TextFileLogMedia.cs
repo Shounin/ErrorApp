e0e4d29435de1da55cdee4703296e066f67a5c41
Garviell <faustusxi@gmail.com>
2014-03-14 16:52:58 +0000

    YEEEES

--- ErrorApp/Code/TextFileLogMedia.cs
+++ ErrorApp/Code/TextFileLogMedia.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Configuration;
+using System.IO;
 
 namespace ErrorApp.Code
 {
     public class TextFileLogMedia :LogMedia
     {
+        string LogFilePath = ConfigurationManager.AppSettings["LogFile"];
+        public override void LogMessage(string message)
+        {
+            using (StreamWriter writer = File.AppendText(LogFilePath))
+            {
+                writer.WriteLine(message);
+            }
+        }
     }
 }
